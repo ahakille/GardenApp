6a25481120f5ce58dab966a81902a9dddc162f47
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-06-11 08:15:19 +0200

    Add getSensors to about component

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { SensorService } from '../../shared/sensor.service';
+import { Sensor } from '../../shared/sensor.model';
+import { Measure } from '../../shared/measue.model';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  sensors: Sensor[] = [];
 
+  constructor(public navCtrl: NavController, private sensorService: SensorService) { }
+
+  getSensors() {
+    this.sensorService.getSensors()
+      .subscribe(sensors => {
+        this.sensors = sensors;
+      });
   }
 
 }
