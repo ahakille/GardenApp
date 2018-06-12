fb720277c19c0db3caa24457c94f981fd3438976
Christoffer Karlsson <christoffer.karlsson@invativa.se>
2018-06-12 20:58:05 +0200

    Change sensor service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HttpModule } from '@angular/http'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -24,7 +24,7 @@ import { SensorService } from '../shared/sensor.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/shared/sensor.service.ts
+++ src/shared/sensor.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@angular/core'
-import { Http, Response } from '@angular/http'
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+
+import { Observable } from 'rxjs/observable';
 
 import { Measure } from '../shared/measue.model'
 import { Sensor } from '../shared/sensor.model'
 
 @Injectable()
 export class SensorService {
-    constructor(private http: Http){}
+    constructor(private http: HttpClient){}
 
-    getSensors() {
-        return <Observable<Sensor[]>>this.http
-            .get('http://api.nppc.se/api/values/sensors')
-            .map(res => this.extractData<Sensor[]>(res))
-    }
-
-    private extractData<T>(res: Response) {
-        if (res.status < 200 || res.status >= 300) {
-            throw new Error('Bad response status: ' + res.status);
-        }
-        let body = res.json ? res.json() : null;
-        return <T>(body && body.data || {});
-    }
+    getSensors(): Observable<Sensor[]> {
+        return this.http.get<Sensor[]>('http://api.nppc.se/api/values/sensors');                  
+    }        
 }
